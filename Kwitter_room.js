ed37cd4839312d98989f1df9f3100add2b4dc2bf
Rhea <78015577+Rhea1122@users.noreply.github.com>
2021-08-26 14:18:59 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+apiKey: "AIzaSyCycFEz1x6o26L08f-e1J1Pq8ccrVs72ac",
+authDomain: "kwitter-c-94-7f594.firebaseapp.com",
+databaseURL: "https://kwitter-c-94-7f594-default-rtdb.firebaseio.com",
+projectId: "kwitter-c-94-7f594",
+storageBucket: "kwitter-c-94-7f594.appspot.com",
+messagingSenderId: "204741716914",
+appId: "1:204741716914:web:98b40dca1eaa630f2892ab"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
